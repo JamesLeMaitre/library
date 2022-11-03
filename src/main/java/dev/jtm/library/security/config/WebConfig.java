9f773a2ad96322ac5d1dd25ba498e819a56bf94a
JamesLeMaitre <jamestchao14@gmail.com>
2022-11-03 14:34:19 +0000

    03/11/2022 14:34

--- src/main/java/dev/jtm/library/security/config/WebConfig.java
+++ src/main/java/dev/jtm/library/security/config/WebConfig.java
@@ -2,18 +2,18 @@ package dev.jtm.library.security.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
+   @Override
     public void addCorsMappings(CorsRegistry registry) {
-
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("*");
-
-
+       registry.addMapping("/**")
+               .allowedOrigins("http://localhost:3000")
+               .allowedMethods("GET","POST","PUT","DELETE")
+               .maxAge(3600);
     }
+
 }
 
